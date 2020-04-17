f14d51546a9833b76340ef2a32265541c6a006eb
Fabian Schmitt <fabian.schmitt@advantest.com>
2020-04-17 13:36:25 +0200

    finished additional practice exerc.

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,18 +1,28 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func Repeat(character string) string {
+func Repeat(character string, repeatCount int) string {
 	var repeated string
-	const repeatcount = 5
-	for i := 0; i < repeatcount; i++ {
+	for i := 0; i < repeatCount; i++ {
 		repeated += character
 	}
 	return repeated
 }
 
+// go test -bench=.
+
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", b.N)
 	}
+}
+
+func ExampleRepeat() {
+	repeat := Repeat("a", 5)
+	fmt.Println(repeat)
+	// Output: aaaaa
 }
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -3,10 +3,10 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-    repeated := Repeat("a")
-    expected := "aaaaa"
+	repeated := Repeat("a", 5)
+	expected := "aaaaa"
 
-    if repeated != expected {
-        t.Errorf("expected %q but got %q", expected, repeated)
-    }
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
 }
